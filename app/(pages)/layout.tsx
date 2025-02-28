39f9184e5058433d5c5109759d878f13f4cbf919
MoreForBegs <seikou615@gmail.com>
2025-02-28 18:54:52 +0000

    Add Toaster to layout

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from 'react'
 import { Sidebar } from '@/components'
+import { Toaster } from '@/components/ui'
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
-    <div className='w-screen flex gap-12 items-start'>
+    <div className='w-screen flex gap-12 items-start relative'>
       <Sidebar />
-      {children}
+      <main className='px-10 pt-8 pb-8 w-full h-screen flex flex-col gap-8 overflow-y-auto'>
+        {children}
+        <Toaster />
+      </main>
     </div>
   )
 }
