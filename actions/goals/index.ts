95544e0d031d1b8f7ac0370f7565cf3be40eae5c
MoreForBegs <seikou615@gmail.com>
2025-03-03 14:22:07 +0000

    Add goal actions and export it

---
+++ actions/goals/index.ts
@@ -0,0 +1,39 @@
+'use server'
+
+import { createServer } from '@/lib/supabase'
+import { Goal } from '@/types'
+import { revalidatePath } from 'next/cache'
+
+export async function insertGoal(goalData: Goal) {
+  const supabase = await createServer()
+
+  const { error } = await supabase.from('goals').insert([goalData])
+
+  if (error) throw new Error(error.message)
+
+  revalidatePath('/')
+  revalidatePath('/goals')
+}
+
+export async function getGoals(): Promise<Goal[]> {
+  const supabase = await createServer()
+
+  const { data, error } = await supabase.from('goals').select('*')
+
+  if (error) throw new Error(error.message)
+
+  const goals: Goal[] = data
+
+  return goals
+}
+
+export async function deleteGoal(id: string) {
+  const supabase = await createServer()
+
+  const { error } = await supabase.from('goals').delete().eq('id', id)
+
+  if (error) throw new Error(error.message)
+
+  revalidatePath('/')
+  revalidatePath('/goals')
+}
